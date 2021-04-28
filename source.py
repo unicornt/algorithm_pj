7d282d5490e6dc7e327b5f9d491b9ac92c072489
unicornt <unicorntwin@outlook.com>
2021-04-28 21:04:49 +0800

    finish lab1

--- source.py
+++ source.py
@@ -38,14 +38,14 @@ def calc_diff(s1, s2):
     s1 += '$'
     s2 += '@'
     res = []
-    print(n, m)
+    # print(n, m)
     while i < n and j < m:
         while s1[i] == s2[j]:
             i += 1
             j += 1
         if i >= n and j >= m:
             break
-        print('different', i, j, n, m)
+        # print('different', i, j, n, m)
         f = 0
         for k in range(2, min(m - j, n - i, 1000)):
             # print('k', k)
@@ -59,8 +59,8 @@ def calc_diff(s1, s2):
                 w += 1
             if w < MIN_SAME_LEN:
                 continue
-            res.append(('REV', i, k))
-            print('REV', i, i + k)
+            res.append(('REV', i, i + k))
+            # print('REV', i, i + k)
             i += k
             j += k
             f = 1
@@ -71,8 +71,8 @@ def calc_diff(s1, s2):
                 while(s1[i + k + w] == s2[j + w] and w <= MIN_SAME_LEN):
                     w += 1
                 if w > MIN_SAME_LEN:
-                    res.append(('DEL', i, k))
-                    print('DEL', i, i + k)
+                    res.append(('DEL', i, i + k))
+                    # print('DEL', i, i + k)
                     f = 1
                     i += k
                     break
@@ -81,14 +81,14 @@ def calc_diff(s1, s2):
                     w += 1
                 if w > MIN_SAME_LEN:
                     if s2[j: j + k] == s1[i - k + 1: i + 1]:
-                        res.append(('DUP', i - k + 1, k))
-                        print('DUP', i - k + 1, i)
+                        res.append(('DUP', i - k + 1, i + 1))
+                        # print('DUP', i - k + 1, i)
                         j += k
                         f = 1
                         break
                     else:
-                        res.append(('ADD', i, k))
-                        print('ADD', i, i + k)
+                        res.append(('ADD', i, i + k))
+                        # print('ADD', i, i + k)
                         j += k
                         f = 1
                         break
@@ -96,24 +96,42 @@ def calc_diff(s1, s2):
                 while(s1[i + k + w] == s2[j + k + w] and w <= MIN_SAME_LEN):
                     w += 1
                 if w > MIN_SAME_LEN:
-                    res.append(('TRA', i, k))
-                    print('TRA', i, k)
+                    # print(s1[i:i+k], s2[j:j+k])
+                    res.append(('TRA0', i, k, s1[i:i+k], s2[j:j+k]))
+                    # print('TRA0', i, k)
                     i += k
                     j += k
                     f = 1
                     break
         if f == 0:
-            res.append(('DEL', i, n - i))
-            res.append(('ADD', n, m - j))
+            res.append(('DEL', i, n))
+            res.append(('ADD', n, m))
             i = n
             j = m
-    print(res)
+    # print(res)
+    return res
             
+rest = {}
 
 def solve():
     for x in dic.keys():
-        print(x)
-        calc_diff(dic[x], dic_n[x])
+        # print(x)
+        rest[x] = calc_diff(dic[x], dic_n[x])
+    for x in rest:
+        for y in rest:
+            if x != y:
+                for ui in range(len(rest[x])):
+                    for vi in range(len(rest[y])):
+                        u = rest[x][ui]
+                        v = rest[y][vi]
+                        if u[2] != v[2]:
+                            continue
+                        if u[0] == 'TRA0' and v[0] == 'TRA0' and \
+                            u[3] == v[4] and u[4] == v[3]:
+                            rest[x][ui] = ('TRA', u[1], u[1] + u[2], v[1], v[1] + v[2])
+                            rest[y][vi] = ('TRA', u[1], u[1] + u[2], v[1], v[1] + v[2])
+    for x in rest:
+        print(rest[x])
 
 def main():
     inf = open("./task1/ref.fasta")
@@ -126,7 +144,7 @@ def main():
                 print('error')
             else:
                 dic[sname] = s[:-1]
-    print(dic.keys())
+    # print(dic.keys())
     inf = open("./task1/sv.fasta")
     for s in inf.readlines():
         if s[0] == '>':
@@ -137,7 +155,7 @@ def main():
                 print('error')
             else:
                 dic_n[sname] = s[:-1]
-    print(dic_n.keys())
+    # print(dic_n.keys())
     solve()
 
 if __name__ == "__main__":
