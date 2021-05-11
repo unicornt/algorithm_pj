010d0263dc49791d781aee430c29575a3f482c02
unicornt <unicorntwin@outlook.com>
2021-05-11 10:58:18 +0800

    finish

--- source.py
+++ source.py
@@ -1,3 +1,4 @@
+import functools
 def Ins(s, si, k):
     return s[0:k] + si + s[k:]
 
@@ -12,7 +13,6 @@ def Inv(s, k, l):
 
 dic = {}
 dic_n = {}
-l = {}
 MIN_SAME_LEN = 20
 
 def rev(c):
@@ -25,12 +25,7 @@ def rev(c):
     else:
         return 'C'
 
-def calc_diff(s1, s2):
-    l.clear()
-    l['A'] = []
-    l['C'] = []
-    l['T'] = []
-    l['G'] = []
+def calc_diff(s1, s2, sn):
     n = len(s1)
     m = len(s2)
     i = 0
@@ -59,7 +54,7 @@ def calc_diff(s1, s2):
                 w += 1
             if w < MIN_SAME_LEN:
                 continue
-            res.append(('REV', i, i + k))
+            res.append(('INV', sn, i, i + k))
             # print('REV', i, i + k)
             i += k
             j += k
@@ -68,10 +63,23 @@ def calc_diff(s1, s2):
         if f == 0:
             for k in range(1, min(m - j, n - i, 1000)):
                 w = 0
+                while(s1[i + w] == s2[j + k + w] and w <= MIN_SAME_LEN):
+                    w += 1
+                if w > MIN_SAME_LEN:
+                    for ki in range(0, 20):
+                        if s2[j - k: j - ki] == s2[j - ki: j + k - ki]:
+                            res.append(('DUP', sn, i - k + 1, i - ki + 1))
+                            # print('DUP', i - k + 1, i)
+                            j += k - ki
+                            f = 1
+                            break
+        if f == 0:
+            for k in range(1, min(m - j, n - i, 1000)):
+                w = 0
                 while(s1[i + k + w] == s2[j + w] and w <= MIN_SAME_LEN):
                     w += 1
                 if w > MIN_SAME_LEN:
-                    res.append(('DEL', i, i + k))
+                    res.append(('DEL', sn, i, i + k))
                     # print('DEL', i, i + k)
                     f = 1
                     i += k
@@ -80,18 +88,11 @@ def calc_diff(s1, s2):
                 while(s1[i + w] == s2[j + k + w] and w <= MIN_SAME_LEN):
                     w += 1
                 if w > MIN_SAME_LEN:
-                    if s2[j: j + k] == s1[i - k + 1: i + 1]:
-                        res.append(('DUP', i - k + 1, i + 1))
-                        # print('DUP', i - k + 1, i)
-                        j += k
-                        f = 1
-                        break
-                    else:
-                        res.append(('ADD', i, i + k))
-                        # print('ADD', i, i + k)
-                        j += k
-                        f = 1
-                        break
+                    res.append(('INS', sn, i, i + k))
+                    # print('ADD', i, i + k)
+                    j += k
+                    f = 1
+                    break
                 w = 0
                 while(s1[i + k + w] == s2[j + k + w] and w <= MIN_SAME_LEN):
                     w += 1
@@ -105,18 +106,37 @@ def calc_diff(s1, s2):
                     break
         if f == 0:
             res.append(('DEL', i, n))
-            res.append(('ADD', n, m))
+            res.append(('INS', n, m))
             i = n
             j = m
     # print(res)
     return res
             
 rest = {}
+prio = {"DUP": 0, "INS": 1, "DEL": 1, "INV": 2, "TRA": 3}
+
+def cmp(a, b):
+    if prio[a[0]] == prio[b[0]]:
+        if a[1] == b[1]:
+            if a[2] < b[2]:
+                return -1
+            else:
+                return 1
+        else:
+            if a[1] < b[1]:
+                return -1
+            else:
+                return 1
+    else:
+        if prio[a[0]] < prio[b[0]]:
+            return -1
+        else:
+            return 1
 
 def solve():
     for x in dic.keys():
         # print(x)
-        rest[x] = calc_diff(dic[x], dic_n[x])
+        rest[x] = calc_diff(dic[x], dic_n[x], x)
     for x in rest:
         for y in rest:
             if x != y:
@@ -128,13 +148,32 @@ def solve():
                             continue
                         if u[0] == 'TRA0' and v[0] == 'TRA0' and \
                             u[3] == v[4] and u[4] == v[3]:
-                            rest[x][ui] = ('TRA', u[1], u[1] + u[2], v[1], v[1] + v[2])
-                            rest[y][vi] = ('TRA', u[1], u[1] + u[2], v[1], v[1] + v[2])
+                            rest[x][ui] = ('TRA', x, u[1], u[1] + u[2], y, v[1], v[1] + v[2])
+                            rest[y][vi] = ('TRA2222', x, u[1], u[1] + u[2], y, v[1], v[1] + v[2])
+    inf = open("./task1-sample/sv_ans.bed", 'w')
+    res=[]
     for x in rest:
-        print(rest[x])
+        for y in rest[x]:
+            if y[0] == 'TRA2222':
+                continue
+            if y[0] == 'TRA0':
+                y[0] = 'ADD'
+                res.append(y)
+                y[0] = 'DEL'
+            res.append(y)
+    res.sort(key=functools.cmp_to_key(cmp))
+    print(res)
+    ret = []
+    for y in res:
+        s = ''
+        for i in range(len(y)):
+            s += str(y[i]) + ' \n'[i == len(y) - 1]
+        ret.append(s)
+    for s in ret:
+        inf.write(s)
 
 def main():
-    inf = open("./task1/ref.fasta")
+    inf = open("./task1-sample/ref.fasta")
     for s in inf.readlines():
         if s[0] == '>':
             # name of sequence
@@ -145,7 +184,7 @@ def main():
             else:
                 dic[sname] = s[:-1]
     # print(dic.keys())
-    inf = open("./task1/sv.fasta")
+    inf = open("./task1-sample/sv.fasta")
     for s in inf.readlines():
         if s[0] == '>':
             # name of sequence
